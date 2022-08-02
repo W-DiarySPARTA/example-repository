630924f9218642ed62764a73fb985d26fb2019cc
blueguy42 <ahmadalfanihandoyo@hotmail.com>
2022-08-03 00:21:59 +0700

    main page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Welcome, Kelompok W!</h1>
+        <h4>Mentor-mentor terkece:</h4>
+        <p>Ahmad Alfani Handoyo<br></br>
+        Dewa Ayu Mutiara K. P. D.<br></br>
+        Natanael Dias
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>SPARTANS terasiek:</h4>
+        <p>
+        Fakhri Muhammad Mahendra<br></br>
+        Rifqi Syafiq<br></br>
+        Jauza Lathifah Annassalafi<br></br>
+        Raditya Azka Prabaswara<br></br>
+        Fakih Anugerah Pratama<br></br>
+        Reswara Trista Aulia Candrakanti<br></br>
+        Adrian Fahri Affandi<br></br>
+        Bill Clinton<br></br>
+        Josua Adriel Sinabutar<br></br>
+        Timothy Subekti<br></br>
+        Muhammad Aliefnaufal<br></br>
+        Nazhif Haidar Putra Wibowo<br></br>
+        Noel Christoffel Simbolon<br></br>
+        Laila Bilbina Khoiru Nisa
+        </p>
       </header>
     </div>
   );
